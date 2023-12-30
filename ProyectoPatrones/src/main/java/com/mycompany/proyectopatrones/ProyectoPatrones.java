99f22463aa40154270f8b8825ad0f968b793994f
SergiodeAndres <91533687+SergiodeAndres@users.noreply.github.com>
2023-12-30 11:30:58 +0100

    Prueba

---
+++ ProyectoPatrones/src/main/java/com/mycompany/proyectopatrones/ProyectoPatrones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectopatrones;
+
+/**
+ *
+ * @author sergi
+ */
+public class ProyectoPatrones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
